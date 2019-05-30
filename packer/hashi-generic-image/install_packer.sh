746c750d976e637158845efd1324b4b1ef737ad5
Raymond Kao <rakao@microsoft.com>
2019-05-30 00:56:52 -0400

    using apt-get for stable scripting api

--- packer/hashi-generic-image/install_packer.sh
+++ packer/hashi-generic-image/install_packer.sh
@@ -5,8 +5,8 @@
 export PACKER_ZIPFILE="packer_"$PACKER_VERSION"_linux_amd64.zip"
 export PACKER_URL="https://releases.hashicorp.com/packer/"$PACKER_VERSION"/"$PACKER_ZIPFILE
 
-apt update
-apt install -y unzip wget
+apt-get update
+apt-get install -y unzip wget
 
 wget $PACKER_URL
 unzip $PACKER_ZIPFILE
